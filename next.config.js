9aa8e33497504a746d9bb2d3a940c282c9a84454
Eduardo Gatti <eduardo.gf@hotmail.com>
2025-04-27 10:12:23 -0600

    fix: remove distDir and rebuild properly

--- next.config.js
+++ next.config.js
@@ -15,14 +15,12 @@ const nextConfig = {
       'picsum.photos',
       'nutritionsource.hsph.harvard.edu',
       "m.media-amazon.com",
-
     ],
   },
-}
-
+};
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
-})
+});
 
-module.exports = withBundleAnalyzer(nextConfig)
+module.exports = withBundleAnalyzer(nextConfig);
