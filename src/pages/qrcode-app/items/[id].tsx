05d813e9bb5244e99225a69eeea7c0f5754fc25e
Eduardo Gatti <eduardo.gf@hotmail.com>
2025-05-26 16:17:05 -0600

    fix menu

--- src/pages/qrcode-app/items/[id].tsx
+++ src/pages/qrcode-app/items/[id].tsx
@@ -1,14 +1,13 @@
-import { Container, ImageCrop, MainContent, WrapperBody } from '../../../styles/qrcode';
-import { useEffect, useState } from 'react';
+import { Container, ImageCrop, MainContent } from '../../../styles/qrcode';
 
 import { Header } from '../../../components/_ui/Header';
-import { MainLayout } from '../../../layouts/MainLayout';
-import { MenuBar } from '../../../components/_ui/MenuBar';
+import { PrivateLayout } from '../../../layouts/PrivateLayout';
 import { UploadPhotoWithCrop } from '../../../components/_ui/UploadPhotoWithCrop';
 import graphqlRequestClient from '../../../lib/graphql.request';
 import { uploadImages } from '../../../lib/imgBB';
 import { useCreateItemMutation } from '../../../generated/graphql';
 import { useRouter } from 'next/router';
+import { useState } from 'react';
 
 const AddItemPage = () => {
     const router = useRouter();
@@ -29,7 +28,7 @@ const AddItemPage = () => {
             return;
         }
 
-        if(imageUrl && !imageUrl.includes('imgbb')) {
+        if (imageUrl && !imageUrl.includes('imgbb')) {
             const { data } = await uploadImages(imageUrl);
             setImageUrl(data.url);
         }
@@ -55,46 +54,42 @@ const AddItemPage = () => {
 
 
     return (
-     <MainLayout
-       headTitle="Scan QR Code"
-       metaContent="Scan QR Code to find a container"
-       metaName="description"
-     >
-       <Container>
-         <Header />
-         <WrapperBody>
-           {/* <MenuBar /> */}
- 
-           <MainContent>
-        <div className="min-h-screen bg-gray-100 p-4">
-            <div className="max-w-md mx-auto bg-white p-6 rounded-md shadow-md">
+        <PrivateLayout
+            title="Scan QR Code"
+            description="Scan QR Code to find a container"
 
-        <h1 className="text-2xl font-bold text-gray-800 mb-6">Add Item</h1>
-                <form onSubmit={handleSubmit} className="space-y-4">
-                    <div>
-                        <label className="block text-sm font-medium text-gray-700">Item Name:</label>
-                        <input
-                            type="text"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                            placeholder="Enter item name"
-                            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                            required
-                        />
-                    </div>
-                    <div>
-                        <label className="block text-sm font-medium text-gray-700">Description:</label>
-                        <textarea
-                            value={description}
-                            onChange={(e) => setDescription(e.target.value)}
-                            placeholder="Enter item description"
-                            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                            required
-                        ></textarea>
-                    </div>
-                    <div>
-                        <label className="block text-sm font-medium text-gray-700">Image URL:</label>
-                        {/* <input
+        >
+            <Container>
+                <MainContent>
+                    <div className="min-h-screen">
+                        <div className="max-w-md bg-white p-6 rounded-md shadow-md">
+
+                            <h1 className="text-2xl font-bold text-gray-800 mb-6">Add Item</h1>
+                            <form onSubmit={handleSubmit} className="space-y-4">
+                                <div>
+                                    <label className="block text-sm font-medium text-gray-700">Item Name:</label>
+                                    <input
+                                        type="text"
+                                        value={name}
+                                        onChange={(e) => setName(e.target.value)}
+                                        placeholder="Enter item name"
+                                        className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                                        required
+                                    />
+                                </div>
+                                <div>
+                                    <label className="block text-sm font-medium text-gray-700">Description:</label>
+                                    <textarea
+                                        value={description}
+                                        onChange={(e) => setDescription(e.target.value)}
+                                        placeholder="Enter item description"
+                                        className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                                        required
+                                    ></textarea>
+                                </div>
+                                <div>
+                                    <label className="block text-sm font-medium text-gray-700">Image URL:</label>
+                                    {/* <input
                             type="url"
                             value={imageUrl}
                             onChange={(e) => setImageUrl(e.target.value)}
@@ -102,62 +97,60 @@ const AddItemPage = () => {
                             className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                             required
                         /> */}
-                        <ImageCrop>
-                        <UploadPhotoWithCrop
-                            setBanner={(value: string) => setImageUrl(value)}
-                            imageUser={imageUrl}
-                        />
-         
-                        </ImageCrop>
+                                    <ImageCrop>
+                                        <UploadPhotoWithCrop
+                                            setBanner={(value: string) => setImageUrl(value)}
+                                            imageUser={imageUrl}
+                                        />
+
+                                    </ImageCrop>
+                                </div>
+                                <div>
+                                    <label className="block text-sm font-medium text-gray-700">Qtt:</label>
+                                    <input
+                                        type="number"
+                                        value={quantity}
+                                        onChange={(e) => setQuantity(parseInt(e.target.value, 10))}
+                                        min="1"
+                                        className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                                        required
+                                    />
+                                </div>
+                                <div>
+                                    <label className="block text-sm font-medium text-gray-700">Category:</label>
+                                    <input
+                                        type="text"
+                                        value={category}
+                                        onChange={(e) => setCategory(e.target.value)}
+                                        placeholder="Enter category"
+                                        className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                                        required
+                                    />
+                                </div>
+                                <div className="flex justify-between space-x-4">
+                                    <button
+                                        type="submit"
+                                        className="w-full px-4 py-2 bg-blue-500 text-white rounded-md shadow-sm hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+                                    >
+                                        Save Item
+                                    </button>
+                                    <button
+                                        type="button"
+                                        onClick={() => router.back()}
+                                        className="w-full px-4 py-2 bg-gray-300 text-black rounded-md shadow-sm hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:ring-offset-2"
+                                    >
+                                        Cancel
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
-                    <div>
-                        <label className="block text-sm font-medium text-gray-700">Qtt:</label>
-                        <input
-                            type="number"
-                            value={quantity}
-                            onChange={(e) => setQuantity(parseInt(e.target.value, 10))}
-                            min="1"
-                            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                            required
-                        />
-                    </div>
-                    <div>
-                        <label className="block text-sm font-medium text-gray-700">Category:</label>
-                        <input
-                            type="text"
-                            value={category}
-                            onChange={(e) => setCategory(e.target.value)}
-                            placeholder="Enter category"
-                            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                            required
-                        />
-                    </div>
-                    <div className="flex justify-between space-x-4">
-                        <button
-                            type="submit"
-                            className="w-full px-4 py-2 bg-blue-500 text-white rounded-md shadow-sm hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
-                        >
-                            Save Item
-                        </button>
-                        <button
-                            type="button"
-                            onClick={() => router.back()}
-                            className="w-full px-4 py-2 bg-gray-300 text-black rounded-md shadow-sm hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:ring-offset-2"
-                        >
-                            Cancel
-                        </button>
-                    </div>
-                </form>
-            </div>
-        </div>
-              </MainContent>
-     
-            </WrapperBody>
-            
-        </Container>
-    </MainLayout>
-    
-        
+                </MainContent>
+
+            </Container>
+        </PrivateLayout>
+
+
     );
 };
 
