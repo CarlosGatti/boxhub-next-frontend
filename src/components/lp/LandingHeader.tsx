10219b2c45311dc71219ac3bcac054e2dcf58627
Eduardo Gatti <eduardo.gf@hotmail.com>
2025-08-31 21:03:48 -0600

    fix number phone

--- src/components/lp/LandingHeader.tsx
+++ src/components/lp/LandingHeader.tsx
@@ -23,13 +23,13 @@ export function LandingHeader() {
           {/* Contact Info */}
           <div className="flex items-center space-x-4">
             <a
-              href="tel:+18573507504"
+              href="tel:+15083136329"
               className="hidden md:flex items-center text-sm text-gray-600 hover:text-orange-600 transition-colors"
             >
               <svg className="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20">
                 <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
               </svg>
-              +1 (857) 350-7504
+              +1 (508) 313-6329
             </a>
             
             <Link
